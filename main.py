23f8242c1accbd8719085c0d4e40ef39c69eabc0
Rohit Tiwari <166986989+Dedrknex8@users.noreply.github.com>
2025-04-29 08:49:50 +0530

    Done with Github Scrapper tool

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import re
+import time
 
 repo = input(" Enter the gituhb username you want to target : ")
 
@@ -14,8 +15,42 @@ driver = webdriver.Chrome(service=service)
 
 driver.get(repo)
 res = driver.find_elements(By.CLASS_NAME, "repo")
+links = []
+final_link = []
+def raw(second_page):
+    driver.get(second_page)
+    get_raw = driver.find_element(By.CLASS_NAME, 'gWqxTd')
+    get_raw.click()
+    src = driver.page_source
+    src = f"{src}"
+    
+    if "password" in src:
+        print(f"password found {second_page}")
+
+
+def loop(next_page):
+    global check
+    driver.get(next_page)
+    new_link = driver.find_elements(By.CLASS_NAME, 'react-directory-truncate')
+    for check in new_link:
+        pass
+    if "py" in check.text:
+        second_page =  f"{next_page}/blob/main/{check.text}"
+        print("raw found")
+        raw(second_page)
+    else:
+        print("Could not find any main.py")
+
+
+
+
+
 
 for elements in res:
-    print(elements.text)
+    links.append(elements.text)
 
+for i in links:
+    next_page = f"{repo}/{i}"
+    final_link.append(next_page)
+    loop(next_page)
 driver.quit()
